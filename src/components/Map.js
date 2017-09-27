df2257524b06bafc65510f0024696efa5868994a
Yonglin Sun <sunyonglincanada@gmail.com>
2017-09-27 16:31:28 -0400

    feat: added info window on google map marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,10 +6,20 @@ import withScriptjs from 'react-google-maps/lib/async/withScriptjs';
 import withGoogleMap from 'react-google-maps/lib/withGoogleMap';
 import GoogleMap from 'react-google-maps/lib/GoogleMap';
 import Marker from 'react-google-maps/lib/Marker';
+import InfoWindow from 'react-google-maps/lib/InfoWindow';
+import FaAnchor from "react-icons/lib/fa/anchor";
 import MapApp from './MapApp'
 
 
 class Map extends React.Component {
+
+    state = {
+        isOpen: false
+    }
+
+    handleOpenInfoWindow = () => {
+        this.setState({isOpen: !this.state.isOpen})
+    }
 
     render() {
 
@@ -18,12 +28,18 @@ class Map extends React.Component {
                 defaultZoom={13}
                 defaultCenter={{ lat: this.props.coordinates.lat, lng: this.props.coordinates.lng }}
                 ref={map => {
-                    window.service = new google.maps.places.PlacesService(map.context['__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED']);
+                    // todo feature of window.service
+                    // window.service = new google.maps.places.PlacesService(map.context['__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED']);
                 }}
             >
                 <Marker
                     position={{ lat: this.props.coordinates.lat, lng: this.props.coordinates.lng }}
-                />
+                    onClick={this.handleOpenInfoWindow}
+                >
+                    {this.state.isOpen? <InfoWindow onCloseClick={this.handleOpenInfoWindow}>
+                        <FaAnchor />
+                    </InfoWindow> : null}
+                </Marker>
                 <MapApp />
             </GoogleMap>
         )
